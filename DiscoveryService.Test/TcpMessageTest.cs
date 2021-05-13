8c7df6a53fd5594408ef7ded883774dbe35435ad
Nikita Bylym <nikitabylym0@gmail.com>
2021-05-13 23:32:23 +0300

    Tests added
    Added  tests for TcpMessage, MulticastMessage, WireReader/Writer

---
+++ DiscoveryService.Test/TcpMessageTest.cs
@@ -0,0 +1,81 @@
+﻿using LUC.DiscoveryService.CodingData;
+using LUC.DiscoveryService.Messages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DiscoveryService.Test
+{
+    /// <summary>
+    /// Сводное описание для UnitTest4
+    /// </summary>
+    [TestClass]
+    public class TcpMessageTest
+    {
+        [TestMethod]
+        public void Writer_null()
+        {
+            TcpMessage tcp = new TcpMessage();
+            MemoryStream ms = new MemoryStream();
+            WireWriter writer = null;
+            var ex = Assert.ThrowsException<ArgumentNullException>(() => tcp.Write(writer));
+            NUnit.Framework.Assert.That(ex.ParamName, NUnit.Framework.Is.EqualTo("writer"));
+        }
+        [TestMethod]
+        public void Message()
+        {
+            uint MessageId = 1111;
+            uint VersionOfProtocol = 2;
+            List<string> GroupsIds=new List<string>{"a","b" };
+            var writ = new StringWriter();
+            writ.WriteLine("TCP message:");
+            writ.WriteLine($"MessageId = {MessageId};\n" +
+                           $"Protocol version = {VersionOfProtocol};");
+            writ.WriteLine($"{nameof(GroupsIds)}:");
+
+            for (Int32 id = 0; id < GroupsIds.Count; id++)
+            {
+                if (id == GroupsIds.Count - 1)
+                {
+                    writ.WriteLine($"{GroupsIds[id]}");
+                }
+                else
+                {
+                    writ.WriteLine($"{GroupsIds[id]};");
+                }
+            }
+
+            var tcp = new TcpMessage(MessageId, VersionOfProtocol, GroupsIds);
+            Assert.AreEqual(writ.ToString(), tcp.ToString());
+        }
+        [TestMethod]
+        public void Message_GroupsIsNULL()
+        {
+           uint MessageId = 1111;
+           uint VersionOfProtocol = 2;
+           List <string>  GroupsIds = new List<string>();
+            var writ = new StringWriter();
+                writ.WriteLine( "TCP message:");
+                writ.WriteLine($"MessageId = {MessageId};\n" +
+                               $"Protocol version = {VersionOfProtocol};");
+                writ.WriteLine($"{nameof(GroupsIds)}:");
+            
+            for (Int32 id = 0; id < GroupsIds.Count; id++)
+            {
+                if (id == GroupsIds.Count - 1)
+                {
+                    writ.WriteLine($"{GroupsIds[id]}");
+                }
+                else
+                {
+                    writ.WriteLine($"{GroupsIds[id]};");
+                }
+            }
+
+            var tcp = new TcpMessage(MessageId, VersionOfProtocol,null);
+            Assert.AreEqual(writ.ToString(), tcp.ToString());
+        }
+    }
+}
---
+++ DiscoveryService.Test/WireReaderWriterTest.cs
@@ -0,0 +1,115 @@
+﻿using LUC.DiscoveryService.CodingData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DiscoveryService.Test
+{
+    /// <summary>
+    /// Testing Wire Read/Write
+    /// </summary>
+    [TestClass]
+    public class WireReaderWriterTest
+    {
+        /// <summary>
+        ///   Testing write/read 
+        /// </summary>
+        [TestMethod]
+        public void Roundtrip()
+        {
+            var someBytes = new Byte[] { 1, 2, 3 };
+            UInt32 test = 35;
+
+            var ms = new MemoryStream();
+            var writer = new WireWriter(ms);
+
+            writer.Write("alpha");
+            writer.Write(test);
+            writer.WriteBytes(someBytes);
+            writer.WriteByteLengthPrefixedBytes(someBytes);
+
+            ms.Position = 0;
+            var reader = new WireReader(ms);
+            Assert.AreEqual("alpha", reader.ReadString());
+            Assert.AreEqual(test, reader.ReadUInt32());
+            CollectionAssert.AreEqual(someBytes, reader.ReadBytes(3));
+            CollectionAssert.AreEqual(someBytes, reader.ReadByteLengthPrefixedBytes());
+        }
+
+        /// <summary>
+        ///   Testing Buffer Overflow with string
+        /// </summary>
+        [TestMethod]
+        public void BufferOverflow_String()
+        {
+            var ms = new MemoryStream(new byte[] { 10, 1 });
+            var reader = new WireReader(ms);
+            Assert.ThrowsException<EndOfStreamException>(() => reader.ReadString());
+        }
+
+        /// <summary>
+        ///   Testing write Big Prefixed Array
+        /// </summary>
+        [TestMethod]
+        public void BytePrefixedArray_TooBig()
+        {
+            var bytes = new byte[byte.MaxValue + 1];
+            var writer = new WireWriter(new MemoryStream());
+            Assert.ThrowsException<ArgumentException>(() => writer.WriteByteLengthPrefixedBytes(bytes));
+        }
+
+        /// <summary>
+        ///   Testing write Not Ascii symbols in string
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WriteString_NotAscii()
+        {
+            var writer = new WireWriter(Stream.Null);
+            writer.Write("δοκιμή"); // test in Greek
+        }
+
+        /// <summary>
+        ///   Testing write big string
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WriteString_TooBig()
+        {
+            var writer = new WireWriter(Stream.Null);
+            writer.Write(new string('a', 256));
+        }
+
+        /// <summary>
+        ///   Testing read Not Ascii symbols in string
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void ReadString_NotAscii()
+        {
+            var ms = new MemoryStream(new byte[] { 1, 255 });
+            var reader = new WireReader(ms);
+            reader.ReadString();
+        }
+
+        /// <summary>
+        ///   Testing write/read IEnumerable string
+        /// </summary>
+        [TestMethod]
+        public void IEnumerable()
+        {
+            List<String> data = new List<String> { "a","c","b" };
+
+                MemoryStream ms = new MemoryStream();
+                WireWriter writer = new WireWriter(ms);
+
+                writer.WriteEnumerable(data);
+
+                ms.Position = 0;
+                WireReader reader = new WireReader(ms);
+                CollectionAssert.AreEquivalent(data, reader.ReadStringList());
+          
+        }
+    }
+}
